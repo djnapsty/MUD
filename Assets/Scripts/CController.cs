94efa2944818d5a8978645832f5778f9bd40ade4
djnapsty <dj_napsty@Justins-MacBook-Pro-2.local>
2017-04-01 22:52:13 -0700

    v.01

---
+++ Assets/Scripts/CController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CController : MonoBehaviour {
+
+	public float cSpeed;
+	private Rigidbody rb;
+
+
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * cSpeed);
+
+
+	}
+}
---
+++ Assets/Scripts/Cameracontroller.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Cameracontroller : MonoBehaviour {
+
+	public Transform target;
+	public float looksmooth = .09f;
+	public Vector3 offsetFromTarget = new Vector3 (0, 6, -8);
+	public float xTilt = 10f;
+
+	Vector3 destination = Vector3.zero;
+	CharMover mover;
+	float rotationSpeed = 0;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		setCameraTarget (target);
+
+	}
+
+	void setCameraTarget (Transform t) {
+		target = t;
+
+		mover = target.GetComponent<CharMover>();
+
+
+				
+	}
+
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		moveToTarget ();
+		LookAtTarget ();
+	}
+	void moveToTarget () {
+		destination = mover.TargetRotation * offsetFromTarget;
+		destination += target.position;
+		transform.position = destination;
+	}
+
+	void LookAtTarget()
+	{
+		float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotationSpeed, looksmooth);
+		transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle,0);
+
+	}
+
+
+}
